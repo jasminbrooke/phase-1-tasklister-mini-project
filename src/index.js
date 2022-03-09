a12a912121b7c7293cc51f4b13585ef09b7f375d
+ <+>
2022-03-09 10:19:34 -0500

    finished deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let userInput = document.getElementById("new-task-description")
+  let button = document.querySelector('input[type=submit]')
+  let tasklist = document.getElementById("tasks")
+  button.addEventListener("click", (e) =>{
+    e.preventDefault()
+    if(userInput.value) {
+      let li = document.createElement('li')
+      li.textContent = userInput.value
+      tasklist.appendChild(li)
+    }
+    userInput.value = ''
+  })
 });
